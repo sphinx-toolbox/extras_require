9cea4dfea75ab8018b12ce17c39e9555eeb9d35c
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-08-18 22:18:32 +0100

    Updated Sphinx configuration.

--- doc-source/conf.py
+++ doc-source/conf.py
@@ -34,26 +34,25 @@ language = 'en'
 package_root = "sphinxcontrib/extras_require"
 
 extensions = [
-		'sphinx.ext.intersphinx',
-		'sphinx.ext.autodoc',
-		'sphinx.ext.mathjax',
-		'sphinx.ext.viewcode',
-		'sphinxcontrib.httpdomain',
-		"sphinxcontrib.extras_require",
-		"sphinx.ext.todo",
-		"sphinxemoji.sphinxemoji",
-		"notfound.extension",
-		"sphinx_tabs.tabs",
-		"sphinx-prompt",
-		"sphinx_autodoc_typehints",
-		"sphinx.ext.autosummary",
-		"autodocsumm",
-		"sphinx_copybutton",
-		"sphinxcontrib.default_values",
-		"sphinxcontrib.toctree_plus",
-		# "sphinx_gitstamp",
-		'sphinx.ext.autosummary',
-		]
+	'sphinx.ext.intersphinx',
+	'sphinx.ext.autodoc',
+	'sphinx.ext.mathjax',
+	'sphinx.ext.viewcode',
+	'sphinxcontrib.httpdomain',
+	'sphinxcontrib.extras_require',
+	'sphinx.ext.todo',
+	'sphinxemoji.sphinxemoji',
+	'notfound.extension',
+	'sphinx_tabs.tabs',
+	'sphinx-prompt',
+	'sphinx.ext.autosummary',
+	'autodocsumm',
+	'sphinx_copybutton',
+	'sphinxcontrib.default_values',
+	'sphinxcontrib.toctree_plus',
+	'sphinx_autodoc_typehints',
+	'sphinx.ext.autosummary',
+	]
 
 sphinxemoji_style = 'twemoji'
 todo_include_todos = bool(os.environ.get("SHOW_TODOS", 0))
@@ -119,6 +118,12 @@ autodoc_default_options = {
 				"__parameters__",
 				"__subclasshook__",
 				"__init_subclass__",
+				"__attrs_attrs__",
+				"__init__",
+				"__new__",
+				"__getnewargs__",
+				"__abstractmethods__",
+				"__hash__",
 				])
 		}
 
