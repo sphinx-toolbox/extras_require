2a50b152e9b1616af34b917e16f000c1325ab948
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-09-09 21:39:33 +0100

    Bump version: 0.1.1 → 0.2.0

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -39,7 +39,7 @@ __copyright__ = """
 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
 """
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 modname = "extras_require"
 pypi_name = "extras_require"
 import_name = "sphinxcontrib.extras_require"
