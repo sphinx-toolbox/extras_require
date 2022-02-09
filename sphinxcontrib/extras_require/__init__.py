51d99e4fa453af1f9fada66f47c9c3b0bf941117
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-02-09 10:10:44 +0000

    Bump version v0.4.1 -> v0.4.1.post1

--- sphinxcontrib/extras_require/__init__.py
+++ sphinxcontrib/extras_require/__init__.py
@@ -50,7 +50,7 @@ from sphinxcontrib.extras_require.sources import sources
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020 Dominic Davis-Foster"
 __license__: str = "BSD"
-__version__: str = "0.4.1"
+__version__: str = "0.4.1.post1"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = ["extras_require_purger", "setup"]
