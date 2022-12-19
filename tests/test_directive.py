a3e3c6b52151f4d07f59c389a590eacabc28ae44
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2022-12-19 11:39:08 +0000

    Packaging now errors on legacy requirements (3.7+ only)

--- tests/test_directive.py
+++ tests/test_directive.py
@@ -1,6 +1,7 @@
 # Some text strings from https://github.com/pypa/packaging/blob/master/tests/test_requirements.py
 
 # stdlib
+import sys
 import warnings
 from typing import Dict, List
 
@@ -103,16 +104,25 @@ def test_validate_requirements(requirements: List[str], valid_requirements: List
 		)
 def test_validate_requirements_warning(requirements: List[str], valid_requirements: List[str]) -> None:
 
-	warning_msg = "Creating a LegacyVersion has been deprecated and will be removed in the next major release"
+	if sys.version_info < (3, 7):
+		# Old packaging version
+		warning_msg = "Creating a LegacyVersion has been deprecated and will be removed in the next major release"
 
-	with warnings.catch_warnings():
-		warnings.simplefilter("always", DeprecationWarning)
+		with warnings.catch_warnings():
+			warnings.simplefilter("always", DeprecationWarning)
 
-		with pytest.warns(DeprecationWarning, match=warning_msg):
-			assert validate_requirements(requirements) == valid_requirements
+			with pytest.warns(DeprecationWarning, match=warning_msg):
+				assert validate_requirements(requirements) == valid_requirements
 
-	with pytest.raises(ValueError, match=f"Invalid requirement .*: {warning_msg}"):
-		validate_requirements(requirements)
+		with pytest.raises(ValueError, match=f"Invalid requirement .*: {warning_msg}"):
+			validate_requirements(requirements)
+	else:
+		# New packaging version
+		with pytest.raises(
+				ValueError,
+				match=rf"Invalid requirement .*: Expected end or semicolon \(after version specifier\)"
+				):
+			validate_requirements(requirements)
 
 
 @pytest.mark.parametrize(
